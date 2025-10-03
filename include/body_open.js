2f74aaa3ead1b5642028a92fdff9ea93a32c98c3
hetasvi-patel <143477329+hetasvi-patel@users.noreply.github.com>
2025-10-03 15:08:09 +0530

    Add custom alert modal HTML structure

---
+++ include/body_open.js
@@ -0,0 +1,15 @@
+<!-- Alert Popup -->
+<div class="modal fade" id="customAlertModal" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Alert</h5>
+      </div>
+      <div class="modal-body" id="customAlertMessage"></div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" id="customAlertOk">OK</button>
+          <button type="button" class="btn btn-secondary" id="customAlertCancel">Cancel</button>
+      </div>
+    </div>
+  </div>
+</div>
